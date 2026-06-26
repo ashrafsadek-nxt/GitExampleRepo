136cc1948de8295dc4fecfbcd998df993b505c7a
ashraf <ashrafsadekas@outlook.com>
2026-06-26 10:02:12 +0300

    Add project files.

---
+++ GitExample/Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace GitExample.Models
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public bool isCompleted { get; set; }
+    }
+}
